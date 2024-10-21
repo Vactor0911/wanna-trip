fd561270dd6fc846e6b29949e640dda7f29f5ac7
Vactor0911 <vactor0911@gmail.com>
2024-10-21 22:25:32 +0900

    Added main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Main } from "./pages"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,10 @@
+const Main = () => {
+  return (
+    <>
+      <h1>Main Page</h1>
+      <p>This is a main page.</p>
+    </>
+  );
+};
+
+export default Main;
