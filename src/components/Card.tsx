a76d9b52cd3c83af61dab1125dc188223e1d6ef0
costar011 <dnaer456@@gmail.com>
2024-11-29 12:53:29 +0900

    카드 컴포넌트 크기 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ import AddIcon from "@mui/icons-material/Add"; // 플러스 아이콘 추가
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"; // 복사 아이콘 추가
 import DeleteIcon from "@mui/icons-material/Delete"; // 쓰레기통 아이콘 추가
 import MenuIcon from "@mui/icons-material/Menu"; // 메뉴 아이콘 추가
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 
 // Props 타입 정의
 interface CardProps {
@@ -18,16 +19,17 @@ interface CardProps {
 }
 
 const CardStyle = styled.div`
-  width: 300px;
+  width: 14em;
   background-color: #D9D9D9;
   padding: 10px;
-  margin-right: 20px;
+  margin-right: 10px;
   border-radius: 8px;
 
-  h2.header {
-    font-size: 1.2rem;
-    color: #1E1E1E;
-    margin-bottom: 10px;
+  .os-scrollbar {
+    --os-size: 10px;
+    --os-handle-bg: white;
+    --os-handle-border-radius: 2px;
+    --os-handle-border: 1px solid #aaa;
   }
 
   card-menu {
@@ -39,12 +41,12 @@ const CardStyle = styled.div`
 const Card: React.FC<CardProps> = ({ day, plans }) => {
   return (
     <CardStyle>
+      
       <div className="card-menu">
         <span
           style={{
             color: "black",
-            fontSize: "2em",
-            marginLeft: "10px", // 여기에서 왼쪽 여백을 추가
+            fontSize: "1.3em",
           }}
         >
           {day}
@@ -52,28 +54,29 @@ const Card: React.FC<CardProps> = ({ day, plans }) => {
         <AddIcon
           sx={{
             color: "#black",
-            transform: "scale(1.5)",
-            marginLeft: "70px",
+            transform: "scale(1.2)",
+            marginLeft: "60px",
             cursor: "pointer",
           }}
         />
         <ContentCopyIcon
           sx={{
             color: "#black",
-            transform: "scale(1.2)",
-            marginLeft: "15px",
+            transform: "scale(1)",
             cursor: "pointer",
+            marginLeft: "0.3em",
           }}
         />
         <DeleteIcon
           sx={{
             color: "#black",
-            transform: "scale(1.2)",
-            marginLeft: "15px",
+            transform: "scale(1)",
             cursor: "pointer",
+            marginLeft: "0.3em",
           }}
         />
       </div>
+      
       {plans.map((plan, index) => (
         <Content
           key={index}
