4e98bbf859d5f5e89eb7e7b584c33e54a0b7849a
Vactor0911 <vactor0911@gmail.com>
2024-12-21 12:52:36 +0900

    백엔드 환경에 맞게 코드 이식

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,16 +1,19 @@
 import { atom } from "jotai";
+import { TemplateProps } from "../utils/theme";
 
-// 서버 주소와 포트 번호를 설정합니다.
-export const SERVER_HOST = 'http://localhost:3005'; // AXIOS 통신 할 서버 주소 http://localhost:3005
+// 백엔드 서버 주소
+export const SERVER_HOST = 'https://wanna-trip.vactor0911.dev'; // AXIOS 통신 할 서버 주소
 
-
-// LocalStorage에서 상태를 불러옵니다.
+// LocalStorage에서 상태를 불러오기
 const savedLoginState = JSON.parse(localStorage.getItem("WannaTriploginState") || "{}");
 
-export const WannaTripLoginStateAtom = atom({
+export const wannaTripLoginStateAtom = atom({
   isLoggedIn: savedLoginState.isLoggedIn || false, // 로그인 상태
   email: savedLoginState.email || "", // 로그인된 사용자의 이메일
   loginType: savedLoginState.loginType || "normal", // 로그인 타입 ; ENUM(normal, kakao, google)
   loginToken: savedLoginState.loginToken || "", // 로그인 토큰
   refreshToken: savedLoginState.refreshToken || "", // 리프레시 토큰
 });
+
+// 템플릿 정보 불러오기
+export const templateDataAtom = atom(null as TemplateProps | null);
