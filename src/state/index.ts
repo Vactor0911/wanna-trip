e98eef92b85b1d9e9eb5a808fd1c6bbd264edb12
Vactor0911 <vactor0911@gmail.com>
2024-12-21 15:32:06 +0900

    템플릿 임시 데이터 삽입

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,11 +1,13 @@
 import { atom } from "jotai";
-import { TemplateProps } from "../utils/theme";
+import { Template } from "../utils";
 
 // 백엔드 서버 주소
-export const SERVER_HOST = 'https://wanna-trip.vactor0911.dev'; // AXIOS 통신 할 서버 주소
+export const SERVER_HOST = "https://wanna-trip.vactor0911.dev"; // AXIOS 통신 할 서버 주소
 
 // LocalStorage에서 상태를 불러오기
-const savedLoginState = JSON.parse(localStorage.getItem("WannaTriploginState") || "{}");
+const savedLoginState = JSON.parse(
+  localStorage.getItem("WannaTriploginState") || "{}"
+);
 
 export const wannaTripLoginStateAtom = atom({
   isLoggedIn: savedLoginState.isLoggedIn || false, // 로그인 상태
@@ -16,4 +18,5 @@ export const wannaTripLoginStateAtom = atom({
 });
 
 // 템플릿 정보 불러오기
-export const templateDataAtom = atom(null as TemplateProps | null);
+const tempTemplateData = new Template(1, "서울 여행", []);
+export const templateDataAtom = atom(tempTemplateData);
