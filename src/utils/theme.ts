41e1593b6b7b15a03b5d26245f74ba75d92a621c
Vactor0911 <vactor0911@gmail.com>
2024-12-21 12:52:16 +0900

    템플릿, 보드, 카드 인터페이스 구성

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -5,3 +5,34 @@ export const color = {
   primaryDark: "#2d405e",
   link: "#3575f1",
 };
+
+// 템플릿 인터페이스
+export interface TemplateProps {
+  id: number;
+  title: string;
+  boards: BoardProps[];
+}
+
+// 보드 인터페이스
+export interface BoardProps {
+  day: number;
+  cards?: CardProps[];
+}
+
+// 카드 인터페이스
+export enum CardType {
+  TEXT = 0,
+  PLACE = 1,
+}
+
+export interface CardProps {
+  id: number;
+  type: CardType;
+  content: string;
+  startTime: Date;
+  endTime: Date;
+  position: {
+    board: number;
+    top: number;
+  }
+}
