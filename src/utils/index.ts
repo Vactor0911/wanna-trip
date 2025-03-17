dfbf8bd8a45ec35abf91c1999748efd8775369af
Vactor0911 <vactor0911@gmail.com>
2025-03-17 14:13:04 +0900

    MUI 테마 생성

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,44 @@
+import { createTheme } from "@mui/material";
 import dayjs from "dayjs";
 import minMax from "dayjs/plugin/minMax";
 
 dayjs.extend(minMax);
+
+// MUI 테마
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#344056",
+    },
+  },
+  typography: {
+    fontFamily: ["Noto Sans KR", "sans-serif"].join(","),
+    h1: {
+      fontSize: "2em",
+    },
+    h2: {
+      fontSize: "1.5em",
+    },
+    h3: {
+      fontSize: "1.17em",
+    },
+    h4: {
+      fontSize: "1em",
+    },
+    h5: {
+      fontSize: "0.83em",
+    },
+    h6: {
+      fontSize: "0.67em",
+    },
+    subtitle1: {
+      fontSize: "1em",
+    },
+    subtitle2: {
+      fontSize: "0.83em",
+    },
+  },
+});
 
 // 색상 테마
 export const color = {
