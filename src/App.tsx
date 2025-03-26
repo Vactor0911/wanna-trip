37ca83b1c5be4d2617a02b1f820a7e94b784f52c
Vactor0911 <vactor0911@gmail.com>
2025-03-26 09:15:49 +0900

    CssBaseline 요소 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Register from "./pages/Register"; // Register 컴포넌트
 import Template from "./pages/Template"; // Template 컴포넌트
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./utils";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter basename="/wanna-trip">
         <Routes>
           <Route path="/" element={<Main />} />
