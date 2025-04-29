b675bbcc2b0837d462bab379975a01156864eea8
L-HITS <leechan753@gmail.com>
2025-04-29 17:19:43 +0900

    TokenRefresher 위치 조정

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ import Template from "./pages/Template"; // Template 컴포넌트
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./utils";
 import { CssBaseline } from "@mui/material";
+import TokenRefresher from "./components/TokenRefresher";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter basename="/wanna-trip">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/template" element={<Template />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <TokenRefresher>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/template" element={<Template />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </TokenRefresher>
       </BrowserRouter>
     </ThemeProvider>
   );
