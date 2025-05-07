56c33694d1a81633b5553cb20a326cc26b35355e
Vactor0911 <vactor0911@gmail.com>
2025-05-07 10:52:53 +0900

    페이지 라우팅 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Main from "./pages/Main";
-import Login from "./pages/Login"; // Login 컴포넌트
-import Register from "./pages/Register"; // Register 컴포넌트
-import Template from "./pages/Template"; // Template 컴포넌트
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./utils";
 import { CssBaseline } from "@mui/material";
 import TokenRefresher from "./components/TokenRefresher";
+import { Login, Main, Register, Template } from "./pages";
 
 function App() {
   return (
