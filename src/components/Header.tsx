a7248dc19b7b5bade04bb4c9ee70e25c6cf099f7
Vactor0911 <vactor0911@gmail.com>
2025-05-08 14:16:27 +0900

    스타일 수정 및 모바일용 네비게이션 메뉴 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@ import {
   AppBar,
   Avatar,
   Badge,
+  Box,
   Button,
+  ClickAwayListener,
+  Collapse,
   Container,
   Divider,
   IconButton,
@@ -11,15 +14,16 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import { theme } from "../utils";
 import { useCallback, useRef, useState } from "react";
 import FaceRoundedIcon from "@mui/icons-material/FaceRounded";
-import { green } from "@mui/material/colors";
+import { grey } from "@mui/material/colors";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 const Links = [
   { text: "템플릿", to: "/template" },
@@ -38,10 +42,11 @@ const MenuLinks = [
 interface StyledLinkProps {
   to: string;
   children: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
 const StyledLink = (props: StyledLinkProps) => {
-  const { to, children, ...others } = props;
+  const { to, children, onClick, ...others } = props;
   return (
     <NavLink
       to={to}
@@ -54,6 +59,7 @@ const StyledLink = (props: StyledLinkProps) => {
         transition: "color 0.3s",
         "&.active": { color: theme.palette.primary.main },
       }}
+      onClick={onClick}
       {...others}
     >
       {children}
@@ -62,17 +68,51 @@ const StyledLink = (props: StyledLinkProps) => {
 };
 
 const Header = () => {
-  const profileAnchorElement = useRef<HTMLButtonElement | null>(null);
+  const location = useLocation();
+  const navigate = useNavigate(); // 네비게이션 훅
+  const profileAnchorElement = useRef<HTMLButtonElement | null>(null); // 프로필 메뉴 앵커 요소
+  const navMenuButtonAnchorElement = useRef<HTMLButtonElement | null>(null); // 네비게이션 메뉴 버튼 앵커 요소
 
-  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false); // 프로필 메뉴 열림 여부
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState(false); // 네비게이션 메뉴 열림 여부
 
+  // 프로필 메뉴 닫기
   const handleProfileMenuClose = useCallback(() => {
     setIsProfileMenuOpen(false);
   }, []);
 
+  // 프로필 메뉴 버튼 클릭
   const handleProfileButtonClick = useCallback(() => {
     setIsProfileMenuOpen((prev) => !prev);
   }, []);
+
+  // 네비게이션 메뉴 닫기
+  const handleNavMenuClose = useCallback(
+    (event: Event | React.SyntheticEvent) => {
+      if (
+        navMenuButtonAnchorElement.current &&
+        navMenuButtonAnchorElement.current.contains(event.target as HTMLElement)
+      ) {
+        return;
+      }
+      setIsNavMenuOpen(false);
+    },
+    []
+  );
+
+  // 네비게이션 메뉴 버튼 클릭
+  const handleNavMenuButtonClick = useCallback(() => {
+    setIsNavMenuOpen((prev) => !prev);
+  }, []);
+
+  // 네비게이션 메뉴 링크 버튼 클릭
+  const handleNavMenuLinkButtonClick = useCallback(
+    (to: string) => {
+      navigate(to);
+      setIsNavMenuOpen(false);
+    },
+    [navigate]
+  );
 
   return (
     <>
@@ -81,69 +121,131 @@ const Header = () => {
         color="info"
         sx={{
           boxShadow: "none",
-          borderBottom: `2px solid ${theme.palette.divider}`,
         }}
       >
-        <Container maxWidth="xl">
-          <Toolbar
-            disableGutters
+        <Box
+          sx={{
+            borderBottom: `2px solid ${theme.palette.divider}`,
+          }}
+        >
+          <Container maxWidth="xl">
+            <Toolbar
+              disableGutters
+              sx={{
+                height: "80px",
+              }}
+            >
+              {/* 로고 */}
+              <StyledLink to="/">
+                <Typography variant="h4" color="primary">
+                  Wanna Trip
+                </Typography>
+              </StyledLink>
+
+              {/* 네비게이션 바 */}
+              <Stack
+                direction="row"
+                gap={{
+                  sm: 3,
+                  md: 5,
+                  lg: 7,
+                  xl: 10,
+                }}
+                justifyContent="center"
+                flexGrow={1}
+                display={{
+                  xs: "none",
+                  sm: "flex",
+                }}
+              >
+                {Links.map((link, index) => (
+                  <StyledLink key={`link-${index}`} to={link.to}>
+                    <Typography variant="h5" fontWeight={500}>
+                      {link.text}
+                    </Typography>
+                  </StyledLink>
+                ))}
+              </Stack>
+
+              {/* 아이콘 버튼 */}
+              <Stack
+                direction="row"
+                gap={1}
+                color="black"
+                justifyContent="flex-end"
+                flexGrow={{
+                  xs: 1,
+                  sm: 0,
+                }}
+              >
+                {/* 라이트/다크 모드 버튼 */}
+                <IconButton color="inherit">
+                  <DarkModeOutlinedIcon fontSize="large" />
+                </IconButton>
+
+                {/* 알림 버튼 */}
+                <IconButton color="inherit">
+                  <Badge badgeContent={100} color="primary" overlap="circular">
+                    <NotificationsNoneOutlinedIcon fontSize="large" />
+                  </Badge>
+                </IconButton>
+
+                {/* 프로필 버튼 */}
+                <IconButton
+                  color="inherit"
+                  ref={profileAnchorElement}
+                  onClick={handleProfileButtonClick}
+                >
+                  <PermIdentityOutlinedIcon fontSize="large" />
+                </IconButton>
+
+                {/* 네비게이션 메뉴 버튼 */}
+                <IconButton
+                  color="inherit"
+                  onClick={handleNavMenuButtonClick}
+                  ref={navMenuButtonAnchorElement}
+                  sx={{
+                    display: {
+                      sm: "none",
+                    },
+                  }}
+                >
+                  <MenuRoundedIcon fontSize="large" />
+                </IconButton>
+              </Stack>
+            </Toolbar>
+          </Container>
+        </Box>
+
+        {/* 모바일용 네비게이션 메뉴 */}
+        <ClickAwayListener onClickAway={handleNavMenuClose}>
+          <Collapse
+            in={isNavMenuOpen}
             sx={{
-              height: "80px",
+              boxShadow: 3,
             }}
           >
-            {/* 로고 */}
-            <StyledLink to="/">
-              <Typography variant="h4" color="primary">
-                Wanna Trip
-              </Typography>
-            </StyledLink>
-
-            {/* 네비게이션 바 */}
-            <Stack
-              direction="row"
-              gap={{
-                sm: 3,
-                md: 5,
-                lg: 7,
-                xl: 10,
-              }}
-              justifyContent="center"
-              flexGrow={1}
-            >
+            <Stack p={2} paddingX={4} gap={2}>
               {Links.map((link, index) => (
-                <StyledLink key={`link-${index}`} to={link.to}>
-                  <Typography variant="h5" fontWeight={500}>
+                <Button
+                  key={`link-${index}`}
+                  onClick={() => handleNavMenuLinkButtonClick(link.to)}
+                  sx={{
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    fontWeight={500}
+                    color={location.pathname == link.to ? "primary" : "black"}
+                  >
                     {link.text}
                   </Typography>
-                </StyledLink>
+                </Button>
               ))}
             </Stack>
-
-            {/* 아이콘 버튼 */}
-            <Stack direction="row" gap={1} color="black">
-              {/* 라이트/다크 모드 버튼 */}
-              <IconButton color="inherit">
-                <DarkModeOutlinedIcon fontSize="large" />
-              </IconButton>
-
-              {/* 알림 버튼 */}
-              <IconButton color="inherit">
-                <Badge badgeContent={100} color="primary" overlap="circular">
-                  <NotificationsNoneOutlinedIcon fontSize="large" />
-                </Badge>
-              </IconButton>
-
-              {/* 프로필 버튼 */}
-              <IconButton
-                color="inherit"
-                ref={profileAnchorElement}
-                onClick={handleProfileButtonClick}
-              >
-                <PermIdentityOutlinedIcon fontSize="large" />
-              </IconButton>
-            </Stack>
-          </Toolbar>
-        </Container>
+          </Collapse>
+        </ClickAwayListener>
       </AppBar>
 
       {/* 프로필 메뉴 */}
@@ -172,13 +274,13 @@ const Header = () => {
             {/* 프로필 이미지 */}
             <Avatar
               sx={{
-                bgcolor: green[500],
+                bgcolor: theme.palette.primary.main,
               }}
             >
               <FaceRoundedIcon
                 fontSize="large"
                 sx={{
-                  color: "black",
+                  color: grey[100],
                 }}
               />
             </Avatar>
@@ -208,8 +310,8 @@ const Header = () => {
                   pl: 2,
                   "&:hover": {
                     "--variant-containedBg": "white",
-                    "--variant-textBg": "black",
-                    "--variant-outlinedBg": "black",
+                    "--variant-textBg": theme.palette.primary.main,
+                    "--variant-outlinedBg": theme.palette.primary.main,
                   },
                   "&:hover > .MuiTypography-root": {
                     color: "white",
