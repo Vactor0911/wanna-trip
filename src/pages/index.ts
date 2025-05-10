6439d7eabd4203231a327cbc9f5dff9e6570f866
JSukhen2 <sv_angel@naver.com>
2025-05-10 22:38:08 +0900

    페이지 라우팅 인트로 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import Main from "./Main";
 import Login from "./Login";
 import Register from "./Register";
 import Template from "./Template";
+import Intro from "./Intro";
 
-export { Main, Login, Register, Template };
+export { Main, Login, Register, Template , Intro};
