c13110ffc8343efdabd024acb13fb2beb75612bf
Vactor0911 <vactor0911@gmail.com>
2025-05-10 16:45:08 +0900

    템플릿 관련 인터페이스 & 상태 추가

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,3 +1,4 @@
+import { Dayjs } from "dayjs";
 import { atom } from "jotai";
 
 // 로그인 상태
@@ -29,3 +30,42 @@ export const wannaTripLoginStateAtom = atom({
 } as LoginState);
 
 export const kakaoLoginStateAtom = atom(""); // 카카오 로그인 code 상태
+
+// 카드 종류 Enum
+export enum CardTypes {
+  TEXT, // 텍스트
+  PLACE, // 장소
+}
+
+// 카드 객체 인터페이스
+export interface CardInterface {
+  uuid: string; // 카드 UUID
+  type: CardTypes; // 카드 종류
+  startTime: Dayjs; // 카드 시작 시간
+  endTime: Dayjs; // 카드 종료 시간
+}
+
+// 보드 객체 인터페이스
+export interface BoardInterface {
+  day: number; // 보드 날짜
+  cards: CardInterface[]; // 카드 목록
+}
+
+// 템플릿 객체 인터페이스
+export interface TemplateInterface {
+  uuid: string | null; // 템플릿 UUID
+  name: string; // 템플릿 이름
+  boards: BoardInterface[]; // 보드 목록
+}
+
+// 템플릿 상태
+export const templateAtom = atom<TemplateInterface>({
+  uuid: null,
+  name: "MyTemplate",
+  boards: [
+    {
+      day: 1,
+      cards: [],
+    },
+  ],
+}); // 보드 상태
