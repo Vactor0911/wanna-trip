b0fa95d5f1691ce8944272fcbf61abeb0da571be
JSukhen2 <sv_angel@naver.com>
2025-05-10 22:51:07 +0900

    주석 추가

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,20 +1,51 @@
-import { theme } from "../utils/index";
+import React, { useCallback, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; //네이게이트를 사용하기 위해 추가
+import { useAtomValue, useSetAtom } from "jotai";
+import { kakaoLoginStateAtom } from "../state";
 
 const Intro = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // 페이지 주소 이동을 위한 navigate 함수 선언
+  const handleLoginButtonClick = useCallback(() => navigate("/login"), []); // 로그인 페이지로 이동
+  const handleStartButtonClick = useCallback(() => navigate("/template"), []); // 템플릿 페이지로 이동
+
+  const setKakaoLoginState = useSetAtom(kakaoLoginStateAtom);
+  useEffect(() => {
+    // 카카오 로그인 이어서 진행
+    if (window.location.href.split("/?/").length > 1) {
+      try {
+        const link = window.location.href.split("/?/")[1];
+        const parsedLink = link.split("&");
+        const pageLink = `/${parsedLink[0]}`;
+
+        // 원래의 로그인 페이지로 이동
+        navigate(pageLink);
+
+        // 카카오 로그인 코드 저장
+        const code = parsedLink[1].substring(5);
+        setKakaoLoginState(code);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (e) {
+        setKakaoLoginState(""); // 카카오 로그인 코드 초기화
+      }
+    } else {
+      setKakaoLoginState(""); // 카카오 로그인 코드 초기화
+    }
+  }, []);
 
   return (
-    <Stack>
+    <>
+      {/* 화면 비율에 따른 형식 지정 */}
       <Stack
-        direction={{ xs: "column", sm: "column" , md: "row" }}
-        alignItems={{ xs: "start", sm: "start", md: "center" }}
+        direction={{ xs: "column", md: "row" }}
+        alignItems={{ xs: "start", md: "center" }}
         sx={{
           pt: { xs: 3, sm: 10, md: 13 },
         }}
       >
         <Stack gap={{ xs: 2, sm: 5, md: 8 }}>
+          {/* 메인 텍스트 */}
           <Stack>
             <Typography
               variant="h1"
@@ -56,6 +87,7 @@ const Intro = () => {
               </Box>
               로 시작!
             </Typography>
+            {/* 메인 텍스트 설명 */}
             <Typography
               sx={{
                 fontSize: { xs: "3.5vw", sm: "1.3rem", md: "1.4rem" },
@@ -66,8 +98,10 @@ const Intro = () => {
             </Typography>
           </Stack>
 
+          {/* 시작 버튼 */}
           <Button
             variant="contained"
+            onClick={handleStartButtonClick}
             sx={{
               width: { xs: "40vw", sm: "30vw", md: "25vw" },
               height: { xs: "3.5em", sm: "3.5em", md: "4em" },
@@ -84,7 +118,7 @@ const Intro = () => {
           </Button>
         </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 };
 
