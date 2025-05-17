c9c0bc772d91e8d33a9ac526f127362d9566fc00
costar011 <dnaer456@gmail.com>
2025-05-18 00:14:01 +0900

    비밀번호 찾기 페이지 생성

---
+++ src/pages/Passwordsearch.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/material";
+
+const Passwordsearch = () => {
+  return (
+    <Stack>
+      <Stack>passwordsearch</Stack>
+    </Stack>
+  );
+};
+
+export default Passwordsearch;
