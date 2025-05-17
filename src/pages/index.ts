85ad4348c2d184bcd742404bc58f8a4c3c93dc1d
JSukhen2 <sv_angel@naver.com>
2025-05-17 15:12:53 +0900

    게시판 연동 및 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import Main from "./Main";
 import Login from "./Login";
 import Register from "./Register";
 import Template from "./Template";
+import Community from "./Community";
 
-export { Main, Login, Register, Template };
+export { Main, Login, Register, Template, Community };
