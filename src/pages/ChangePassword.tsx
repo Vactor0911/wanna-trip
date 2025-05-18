04730b17548dc914baeadfa7ad1d6dd6171f0db5
Vactor0911 <vactor0911@gmail.com>
2025-05-18 13:03:17 +0900

    이메일 입력란 readOnly 구현

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,11 +1,11 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import {
-    Box,
-    Container,
-    IconButton,
-    InputAdornment,
-    Stack,
-    Typography,
+  Box,
+  Container,
+  IconButton,
+  InputAdornment,
+  Stack,
+  Typography,
 } from "@mui/material";
 import OutlinedTextField from "../components/OutlinedTextField";
 import PlainLink from "../components/PlainLinkProps";
@@ -14,124 +14,124 @@ import SectionHeader from "../components/SectionHeader";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-import axiosInstance, { getCsrfToken } from "../utils/axiosInstance";
-import axios from "axios";
-
-
+// import axiosInstance, { getCsrfToken } from "../utils/axiosInstance";
+// import axios from "axios";
 
 const ChangePassword = () => {
-    const [email, setEmail] = useState(""); // 사용자 이메일
-    const [password, setPassword] = useState(""); // 사용자 비밀번호
-    const [passwordConfirm, setPasswordConfirm] = useState(""); // 사용자 비밀번호 재확인
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // 비밀번호 보임/숨김
-    const [isPasswordConfirmVisible, setIsPasswordCheckVisible] = useState(false); // 비밀번호 확인 보임/숨김
+  const [email, setEmail] = useState(""); // 사용자 이메일
+  const [password, setPassword] = useState(""); // 사용자 비밀번호
+  const [passwordConfirm, setPasswordConfirm] = useState(""); // 사용자 비밀번호 재확인
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false); // 비밀번호 보임/숨김
+  const [isPasswordConfirmVisible, setIsPasswordCheckVisible] = useState(false); // 비밀번호 확인 보임/숨김
 
-    // 비밀번호 입력
-    const handleChangePassword = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            setPassword(e.target.value);
-        },
-        []
-    );
+  // 이메일 정보 가져오기
+  useEffect(() => {
+    const newEmail = "test@gmail.com"; // TODO: 이메일 정보 가져오기
+    setEmail(newEmail);
+  }, []);
 
-    // 비밀번호 재입력 입력
-    const handlePasswordConfirmChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            setPasswordConfirm(e.target.value);
-        },
-        []
-    );
+  // 비밀번호 입력
+  const handlePasswordChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    []
+  );
 
-    // 비밀번호 보임/숨김
-    const handlePasswordVisibilityChange = useCallback(() => {
-        setIsPasswordVisible(!isPasswordVisible);
-    }, [isPasswordVisible]);
+  // 비밀번호 재입력 입력
+  const handlePasswordConfirmChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPasswordConfirm(e.target.value);
+    },
+    []
+  );
 
-    // 비밀번호 확인 보임/숨김
-    const handlePasswordConfirmVisibilityChange = useCallback(() => {
-        setIsPasswordCheckVisible(!isPasswordConfirmVisible);
-    }, [isPasswordConfirmVisible]);
+  // 비밀번호 보임/숨김
+  const handlePasswordVisibilityChange = useCallback(() => {
+    setIsPasswordVisible(!isPasswordVisible);
+  }, [isPasswordVisible]);
 
-    return (
-        <Container maxWidth="xs">
-            <Stack minHeight="100vh" justifyContent="center" pb={29.2}>
-                <Stack gap={6}>
-                    {/* 로고 링크 버튼*/}
-                    <PlainLink to="/">
-                        <Typography variant="h3" color="primary" textAlign="center">
-                            Wanna Trip
-                        </Typography>
-                    </PlainLink>
+  // 비밀번호 확인 보임/숨김
+  const handlePasswordConfirmVisibilityChange = useCallback(() => {
+    setIsPasswordCheckVisible(!isPasswordConfirmVisible);
+  }, [isPasswordConfirmVisible]);
 
-                    <Stack gap={1}>
-                        {/* 비밀번호 찾기 헤더 */}
-                        <SectionHeader title="비밀번호 변경" />
+  return (
+    <Container maxWidth="xs">
+      <Stack minHeight="100vh" justifyContent="center" pb={29.2}>
+        <Stack gap={6}>
+          {/* 로고 링크 버튼*/}
+          <PlainLink to="/">
+            <Typography variant="h3" color="primary" textAlign="center">
+              Wanna Trip
+            </Typography>
+          </PlainLink>
 
-                        {/* 이메일 입력란 */}
-                        <Stack gap={1}>
-                            {/* 아이디(이메일) 입력란 (보기만 가능) */}
-                            {/* TODO: 수정필요 */}
+          <Stack gap={1}>
+            {/* 비밀번호 찾기 헤더 */}
+            <SectionHeader title="비밀번호 변경" />
 
-                            <Stack direction="row" gap={1} mt={1}>
-                                <Box flex={1}>
-                                    <OutlinedTextField
-                                        label="아이디(이메일)"
-                                        value={email}
-                                        readOnly
-                                    />
-                                </Box>
-
-                            </Stack>
-                        </Stack>
-
-                        {/* 비밀번호 입력란 */}
-                        <OutlinedTextField
-                            label="비밀번호"
-                            value={password}
-                            onChange={handleChangePassword}
-                            type={isPasswordVisible ? "text" : "password"}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        onClick={handlePasswordVisibilityChange}
-                                        edge="end"
-                                    >
-                                        {isPasswordVisible ? (
-                                            <VisibilityOffIcon />
-                                        ) : (
-                                            <VisibilityIcon />
-                                        )}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-
-                        {/* 비밀번호 재확인 입력란 */}
-                        <OutlinedTextField
-                            label="비밀번호 재확인"
-                            value={passwordConfirm}
-                            onChange={handlePasswordConfirmChange}
-                            type={isPasswordConfirmVisible ? "text" : "password"}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        onClick={handlePasswordConfirmVisibilityChange}
-                                        edge="end"
-                                    >
-                                        {isPasswordConfirmVisible ? (
-                                            <VisibilityOffIcon />
-                                        ) : (
-                                            <VisibilityIcon />
-                                        )}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </Stack>
-                </Stack>
+            {/* 이메일 입력란 */}
+            <Stack gap={1}>
+              <Stack direction="row" gap={1} mt={1}>
+                <Box flex={1}>
+                  <OutlinedTextField
+                    label="아이디(이메일)"
+                    value={email}
+                    readOnly
+                  />
+                </Box>
+              </Stack>
             </Stack>
-        </Container>
-    );
+
+            {/* 비밀번호 입력란 */}
+            <OutlinedTextField
+              label="비밀번호"
+              value={password}
+              onChange={handlePasswordChange}
+              type={isPasswordVisible ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handlePasswordVisibilityChange}
+                    edge="end"
+                  >
+                    {isPasswordVisible ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+
+            {/* 비밀번호 재확인 입력란 */}
+            <OutlinedTextField
+              label="비밀번호 재확인"
+              value={passwordConfirm}
+              onChange={handlePasswordConfirmChange}
+              type={isPasswordConfirmVisible ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handlePasswordConfirmVisibilityChange}
+                    edge="end"
+                  >
+                    {isPasswordConfirmVisible ? (
+                      <VisibilityOffIcon />
+                    ) : (
+                      <VisibilityIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
+  );
 };
 
 export default ChangePassword;
