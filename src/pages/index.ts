2d0baac25e6fc784570a50a67740fc30b5c1ae64
JSukhen2 <sv_angel@naver.com>
2025-05-18 11:14:22 +0900

    "비밀번호 변경" 페이지 pages에 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,8 @@ import Main from "./Main";
 import Login from "./Login";
 import Register from "./Register";
 import PasswordSearch from "./PasswordSearch";
+import PasswordChange from "./PasswordChange.tsx";
 import Template from "./Template";
 import Community from "./Community";
 
-export { Main, Login, Register, PasswordSearch, Template, Community };
+export { Main, Login, Register, PasswordSearch, PasswordChange, Template, Community };
