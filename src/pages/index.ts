78c3c576b9d10fc506827d201802d04b7481d0cf
JSukhen2 <sv_angel@naver.com>
2025-05-18 10:38:01 +0900

    pages "비밀번호 찾기" 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,8 @@
 import Main from "./Main";
 import Login from "./Login";
 import Register from "./Register";
+import PasswordSearch from "./PasswordSearch";
 import Template from "./Template";
 import Community from "./Community";
 
-export { Main, Login, Register, Template, Community };
+export { Main, Login, Register, PasswordSearch, Template, Community };
