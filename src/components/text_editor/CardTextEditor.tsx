2b454168ae3df4688ee48dd0cc392eb52d1acb54
Vactor0911 <vactor0911@gmail.com>
2025-06-04 15:57:39 +0900

    텍스트 에디터 속성값 수정

--- src/components/text_editor/CardTextEditor.tsx
+++ src/components/text_editor/CardTextEditor.tsx
@@ -6,21 +6,21 @@
 import { useState, useEffect, useRef, useMemo } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import {
-	ClassicEditor,
-	Alignment,
-	AutoLink,
-	Autosave,
-	Bold,
-	Essentials,
-	FontColor,
-	ImageEditing,
-	ImageUtils,
-	Link,
-	List,
-	ListProperties,
-	Paragraph,
-	TodoList
-} from 'ckeditor5';
+  ClassicEditor,
+  Alignment,
+  AutoLink,
+  Autosave,
+  Bold,
+  Essentials,
+  FontColor,
+  ImageEditing,
+  ImageUtils,
+  Link,
+  List,
+  ListProperties,
+  Paragraph,
+  TodoList,
+} from "ckeditor5";
 
 import translations from "ckeditor5/translations/ko.js";
 
@@ -67,18 +67,18 @@ export default function CardTextEditor() {
         },
         plugins: [
           Alignment,
-					AutoLink,
-					Autosave,
-					Bold,
-					Essentials,
-					FontColor,
-					ImageEditing,
-					ImageUtils,
-					Link,
-					List,
-					ListProperties,
-					Paragraph,
-					TodoList
+          AutoLink,
+          Autosave,
+          Bold,
+          Essentials,
+          FontColor,
+          ImageEditing,
+          ImageUtils,
+          Link,
+          List,
+          ListProperties,
+          Paragraph,
+          TodoList,
         ],
         blockToolbar: [
           "fontSize",
@@ -97,72 +97,6 @@ export default function CardTextEditor() {
           "outdent",
           "indent",
         ],
-        fontFamily: {
-          supportAllValues: true,
-        },
-        fontSize: {
-          options: [10, 12, 14, "default", 18, 20, 22],
-          supportAllValues: true,
-        },
-        heading: {
-          options: [
-            {
-              model: "paragraph" as const,
-              title: "Paragraph",
-              class: "ck-heading_paragraph",
-            },
-            {
-              model: "heading1" as const,
-              view: "h1",
-              title: "Heading 1",
-              class: "ck-heading_heading1",
-            },
-            {
-              model: "heading2" as const,
-              view: "h2",
-              title: "Heading 2",
-              class: "ck-heading_heading2",
-            },
-            {
-              model: "heading3" as const,
-              view: "h3",
-              title: "Heading 3",
-              class: "ck-heading_heading3",
-            },
-            {
-              model: "heading4" as const,
-              view: "h4",
-              title: "Heading 4",
-              class: "ck-heading_heading4",
-            },
-            {
-              model: "heading5" as const,
-              view: "h5",
-              title: "Heading 5",
-              class: "ck-heading_heading5",
-            },
-            {
-              model: "heading6" as const,
-              view: "h6",
-              title: "Heading 6",
-              class: "ck-heading_heading6",
-            },
-          ],
-        },
-        image: {
-          toolbar: [
-            "toggleImageCaption",
-            "imageTextAlternative",
-            "|",
-            "imageStyle:inline",
-            "imageStyle:wrapText",
-            "imageStyle:breakText",
-            "|",
-            "resizeImage",
-          ],
-        },
-        initialData:
-          '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
         language: "ko",
         licenseKey: LICENSE_KEY,
         link: {
@@ -185,16 +119,7 @@ export default function CardTextEditor() {
             reversed: true,
           },
         },
-        placeholder: "Type or paste your content here!",
-        table: {
-          contentToolbar: [
-            "tableColumn",
-            "tableRow",
-            "mergeTableCells",
-            "tableProperties",
-            "tableCellProperties",
-          ],
-        },
+        placeholder: "내용을 입력해주세요.",
         translations: [translations],
       },
     };
