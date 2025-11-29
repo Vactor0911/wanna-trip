2202ea89199eadfd7560e0c0d595c7eedc19b628
Vactor0911 <vactor0911@gmail.com>
2025-11-29 16:58:16 +0900

    GitHub Pages 대응 리다이렉트 훅 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useTheme, useMediaQuery } from "@mui/material";
+import { useNavigate } from "react-router";
 
 // 소켓 및 알림 훅 export
 export * from "./socket";
@@ -21,4 +22,25 @@ export const useBreakpoint = (): "xs" | "sm" | "md" | "lg" | "xl" => {
   if (isMd) return "md";
   if (isSm) return "sm";
   return "xs";
+};
+
+/**
+ * GitHub Pages에서 404 에러 페이지로 리다이렉트된 경우 URL을 파싱하여 원래의 페이지로 리다이렉트하는 훅
+ */
+export const useRedirectPage = () => {
+  const navigate = useNavigate();
+
+  return (): boolean => {
+    const currentUrl = window.location.href; // 현재 URL 가져오기
+
+    // 404.html에서 반환된 URL의 형식이 아닌 경우 종료
+    if (!currentUrl.includes("/?/")) {
+      return false; // 리다이렉트 실패
+    }
+
+    // 404.html에서 반환된 URL 파싱
+    const url = currentUrl.split("/?/")[1];
+    navigate(url, { replace: true }); // 원래의 페이지로 리다이렉트
+    return true; // 리다이렉트 성공
+  };
 };
